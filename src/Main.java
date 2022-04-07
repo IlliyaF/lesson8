c8e1716348738f40dc248dfd6d8ff0b354efa624
IlliyaF <yiliy749@gmail.com>
2022-04-07 12:11:54 +0300

    Version with Multidimensional map and two loopMethods

--- src/Main.java
+++ src/Main.java
@@ -4,69 +4,82 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ChairMap persMap1 = new ChairMap();  //Announcement Our Map
-
+        ChairMap personMap = new ChairMap();  //Announcement Our Map
+        Person pKey = null;
         //Multidimensional map: Our collection holds information about pet owners, each of whom may have several pets
-        persMap1.put(new Person("Vassa"), Arrays.asList(new Cat("Barsick"), new Cat("Pupsik")));
-        persMap1.put(new Person("Mary"), Arrays.asList(new Cat("Tady"), new Dog("Bob")));
-        persMap1.put(new Person("Ivan"), Arrays.asList(new Cat("Liam"), new Dog("Sas"), new Parrot("Arra")));
+        personMap.put(new Person("Vassa"), Arrays.asList(new Cat("Barsick"), new Cat("Pupsik")));
+        personMap.put(new Person("Mary"), Arrays.asList(new Cat("Tady"), new Dog("Bob")));
+        personMap.put(new Person("Ivan"), Arrays.asList(new Cat("Liam"), new Dog("Sas"), new Parrot("Arra")));
 
         //Show us our Map
-        System.out.println("personMap: " + persMap1);
+        System.out.println("personMap: " + personMap);
 
         //Method toString()
-        System.out.println("personMap.toString(): " + persMap1.toString());
+        System.out.println("personMap.toString(): " + personMap.toString());
 
         //Method keySet()
-        System.out.println("personMap.keySet(): " + persMap1.keySet());
+        System.out.println("personMap.keySet(): " + personMap.keySet());
 
-        for (Person p1 : persMap1.keySet()) {
+        //Method get() - look void method "loopPetValue"
+        loopPersonKey(personMap);
+        /*for (Person p1 : personMap.keySet()) {
             System.out.println(p1.nam + " has");
-            for (Pet pet : persMap1.get(p1)) {
+            for (Pet pet : personMap.get(p1)) {
                 System.out.println("  " + pet + "  " + pet.namePet);
             }
-        }
+        }*/
 
         //Method size()
-        System.out.println("Method size: " + persMap1.size());
+        System.out.println("Method size: " + personMap.size());
 
         List arrPeople = new ArrayList();
-        for (Person p2 : persMap1.keySet()) {
-            arrPeople.add(p2); //Array keys of our Map. It's added for testing method remove()
-            System.out.println("Method contains: " + p2.nam + " " + persMap1.containsKey(p2));
+        for (Person p : personMap.keySet()) {
+            arrPeople.add(p); //Array keys of our Map. It's added for testing method remove()
+            System.out.println("Method contains: " + p.nam + " " + personMap.containsKey(p));
         }
 
-        Person p3 = (Person) arrPeople.get(0); //Generating key for method remove()
+        pKey = (Person) arrPeople.get(0); //Generating key for method remove()
 
         //Method remove
-        persMap1.remove(p3);
-        System.out.println("Removed element Mary - " + p3.nam);
+        personMap.remove(pKey);
+        System.out.println("Removed element Mary - " + pKey.nam);
         System.out.println("Now our Map contains: ");
 
         //Show current state of our Mep - without element Mary
-        for (Person p1 : persMap1.keySet()) {
+        loopPersonKey(personMap);
+        /*for (Person p1 : personMap.keySet()) {
             System.out.println(p1.nam + " has");
-            for (Pet pet : persMap1.get(p1)) {
+            for (Pet pet : personMap.get(p1)) {
                 System.out.println("  " + pet + "  " + pet.namePet);
+            }
+        }*/
+
+        //Method isEmpty() - false
+        System.out.println("Is Map Empty?: " + personMap.isEmpty());
+
+        //Method isEmpty()
+        System.out.println("This is our keys: " + personMap.keysToArray());
+
+        //Method clear()
+        personMap.clear();
+
+        //Method isEmpty() - true
+        System.out.println("Map is Empty after clear: " + personMap.isEmpty());
+
+    }
+    public static void loopPersonKey (ChairMap persMap) {
+        for (Person p : persMap.keySet()) {
+            System.out.println(p.nam + " has");
+            loopPetValue(persMap, p);
             }
         }
 
-        //Method isEmpty() - false
-        System.out.println("Is Map Empty?: " + persMap1.isEmpty());
 
-        //Method isEmpty()
-        System.out.println("This is our keys: " + persMap1.keysToArray());
-
-        //Method clear()
-        persMap1.clear();
-
-        //Method isEmpty() - true
-        System.out.println("Map is Empty after clear: " + persMap1.isEmpty());
-
+    public static void loopPetValue (ChairMap persMap, Person p){
+        for (Pet pet : persMap.get(p)) {
+            System.out.println("  " + pet + "  " + pet.namePet);
+        }
     }
-
-
-
 }
 
 
